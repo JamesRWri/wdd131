f2540c95f2c0af6c4a6df18a118e245fb6ee1d1d
JamesRWri <ninjamez@byui.edu>
2025-10-27 20:46:51 -0600

    Updates Coolpics

--- coolpics/scripts.js
+++ coolpics/scripts.js
@@ -2,12 +2,13 @@ const menuButton = document.getElementById('menuButton');
 const menu = document.getElementById('menu');
 
 menuButton.addEventListener('click', () => {
+  menu.classList.toggle('show');
   menu.classList.toggle('hide');
 });
 
 function handleResize() {
-  if (window.innerWidth > 1000) {
-    menu.classList.remove('hide');
+  if (window.innerWidth >= 900) {
+    menu.classList.remove('hide', 'show');
   } else {
     menu.classList.add('hide');
   }
@@ -15,34 +16,3 @@ function handleResize() {
 
 window.addEventListener('resize', handleResize);
 handleResize();
-
-const gallery = document.querySelector('.gallery');
-let modal;
-
-function viewerTemplate(src, alt) {
-  return `
-    <img src="${src}" alt="${alt}">
-    <button class="close-viewer">X</button>
-  `;
-}
-
-gallery.addEventListener('click', (event) => {
-  const clicked = event.target.closest('img');
-  if (!clicked) return;
-
-  const src = clicked.src.split('-')[0] + '-full.jpg';
-  const alt = clicked.alt;
-
-  modal = document.createElement('dialog');
-  modal.innerHTML = viewerTemplate(src, alt);
-  document.body.appendChild(modal);
-
-  const closeButton = modal.querySelector('.close-viewer');
-  closeButton.addEventListener('click', () => modal.close());
-  modal.addEventListener('click', (e) => {
-    if (e.target === modal) modal.close();
-  });
-
-  modal.addEventListener('close', () => modal.remove());
-  modal.showModal();
-});
